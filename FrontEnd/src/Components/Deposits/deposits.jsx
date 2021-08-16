567698b5051f320147b1a9aeab184005ad0b35be
RAMROG <86012753+RAMROG@users.noreply.github.com>
2021-08-15 23:11:14 -0600

    Update deposits.jsx
    actualizacion 2.3

--- FrontEnd/src/Components/Deposits/deposits.jsx
+++ FrontEnd/src/Components/Deposits/deposits.jsx
@@ -26,32 +26,7 @@ const UseStyles = makeStyles({
 export default function Deposits(props) {
   const classes = UseStyles();
   const bull = <span className={classes.bullet}>•</span>;
-  const [datosPagos, setPagos] = useState();
-  
-  
-  const obtenerPagos = async ()=> {
-     await auth.onAuthStateChanged((z)=>{if(z){
-        const data= async()=>{
-            await database.ref().child(z.uid).child('Pagos').on('value',(e)=>{
-            const todo=0.0;
-            if (e.length>0){
-              e.map(item=>{
-                todo+=item.monto;
-              })
-            }
-            alert("entro")
-            if(todo>=0){
-                setPagos(todo);
-              }   
-        })}
-        data()
-    }else{
-        alert("error")
-    }})
-        }
-  useEffect(() => {
-    obtenerPagos();
-    }, [])
+
 
   return (
     <React.Fragment>
@@ -63,7 +38,7 @@ export default function Deposits(props) {
         <Typography variant="h5" component="h2">
         </Typography>
         <Typography className={classes.pos} color="textSecondary">
-          Movimiento {datosPagos}
+          Movimiento {props.money}
         </Typography>
         <Typography variant="body2" component="p">
           Este es el resumen de:
